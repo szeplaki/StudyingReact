746bab1622a31af64d8a8aaed705bb591075d393
Szeplaki <szeplaki.eva1@gmail.com>
2023-03-09 16:09:46 +0100

    apply two-way bindig in ExpenseForm.js

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -76,20 +76,34 @@ export function ExpenseForm() {
     //         }
     //     });
     // }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        
+        const inputData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+        setEnteredTitle("");
+        setEnteredAmount("");
+        setEnteredDate("");
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
             <div className="new-expense__controls">
                 <label>Title</label>
-                <input type='text' onChange={titleChangeHandler} />
+                <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
             </div>
             <div className="new-expense__controls">
                 <label>Amount</label>
-                <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} />
+                <input type='number' value={enteredAmount} min='0.01' step='0.01' onChange={amountChangeHandler} />
             </div>
             <div className="new-expense__controls">
                 <label>Date</label>
-                <input type='date' min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler} />
+                <input type='date' value={enteredDate} min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler} />
             </div>
             </div>
             <div className="new-expense__actions">

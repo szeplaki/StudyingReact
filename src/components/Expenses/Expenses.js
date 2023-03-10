b14637a034d4ed4a162a0658f30e31baa237c401
Szeplaki <szeplaki.eva1@gmail.com>
2023-03-10 12:56:19 +0100

    practice lifting the state through Expenses.js and ExpenseFilter.js, where later is the controled component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { ExpenseItem } from "./ExpenseItem";
 import { Card } from "../UI/Card";
+import { ExpenseFilter } from "./ExpenseFilter";
 import './Expenses.css';
 
 export function Expenses(expenses) {
+
+    const [year, setYear] = useState("2020");
+    const yearSetter = selectedYear => {
+      setYear(selectedYear);
+    }
+
     return (
         <Card className="expenses">
-          <h2>Let's get started!</h2>
+          <ExpenseFilter selected={year} onChangeYear={yearSetter} />
           <ExpenseItem
             title={expenses.expenses[0].title}
             amount={expenses.expenses[0].amount}

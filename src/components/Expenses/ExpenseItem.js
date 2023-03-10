e204361489c9e9db5d3f7862e7b5d1d2f3b77d9f
Szeplaki <szeplaki.eva1@gmail.com>
2023-03-10 13:23:16 +0100

    make ExpenseItem stateless

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,24 +3,13 @@ import "./ExpenseItem.css";
 import {ExpenseDate} from "./ExpenseDate";
 import { Card } from "../UI/Card";
 export function ExpenseItem(props) {
-
-  const [title, setTitle] = useState(props.title);
-  const handleClick = () => {
-    if (title != props.title) {
-      setTitle(props.title);
-    } else {
-      setTitle("It's working!")
-    }
-    
-  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={handleClick}>Click me please!</button>
     </Card>
   );
 }

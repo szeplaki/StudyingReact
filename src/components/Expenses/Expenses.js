27b881024f0bc657d671264a15bc1c9ad7a2f685
Szeplaki <szeplaki.eva1@gmail.com>
2023-03-12 15:27:02 +0100

    extract conditional content into a new component, and terminate the ternary operator, put list into an ul tag

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { ExpenseItem } from "./ExpenseItem";
+import { ExpensesList } from "./ExpensesList";
 import { Card } from "../UI/Card";
 import { ExpenseFilter } from "./ExpenseFilter";
 import "./Expenses.css";
@@ -12,20 +12,12 @@ export function Expenses(props) {
 
   const filteredExpenses = props.expenses.filter(expense => expense.date.getFullYear().toString() === year);
   
-  let expenseContent = <p>There are no expenses.</p>
+  
 
   return (
     <Card className="expenses">
       <ExpenseFilter selected={year} onChangeYear={yearSetter} />
-      {filteredExpenses.length > 0 ? 
-      expenseContent = filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        /> 
-      )) : expenseContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 }
---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { ExpenseItem } from "./ExpenseItem";
+import "./ExpensesList.css";
+
+export function ExpensesList(props) {
+  if (props.items.length === 0) {
+    return <h2 className="empty-expenses-list">There are no expenses.</h2>;
+  }
+
+  return (
+    <ul className="expenses-list">
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  );
+}
